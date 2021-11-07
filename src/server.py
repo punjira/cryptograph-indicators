95b2e42c9d69013b0e32e90680ea975d6c033616
punjira <saeedwmail@gmail.com>
2021-11-07 13:07:05 -0500

    hide logs

--- src/server.py
+++ src/server.py
@@ -4,6 +4,10 @@ from flask_restful import Resource, Api, reqparse
 import os
 import sys
 
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 sys.path.append(os.path.join(sys.path[0],"endpoints"))
 sys.path.append(os.path.join(sys.path[0],"helpers"))
 
